8bf7c8c1a7d71e9b810de5b9e88db44e7a2a5b10
Najam <mnajams166@gmail.com>
2023-06-07 12:33:36 +0500

    changes

--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,30 @@
+'use client'
+import { useState, useEffect } from "react"
 import Link from "next/link"
+import LoadingPage from "./loading"
 import Courses from "./components/Courses"
 
 const HomePage = () => {
+  const [courses, setCourses] = useState()
+  const [loading, setLoading] = useEffect(true)
+
+  useEffect(() => {
+    const fetchCourses = async () => {
+      const res = await fetch('/api/courses')
+      const data = await res.json();
+      setCourses()
+      setLoading(false)
+    }
+  }, [])
+
+  if (loading){
+    return <LoadingPage />
+  }
+
   return (        
     <div>
       <h1>Welcome to Techno Spot</h1>
-      <Courses />
+      
     </div>
   )       
 }
